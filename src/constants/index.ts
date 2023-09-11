61ef7794240652dd12c60c3dcde1bb3245361db9
ShinJaeIl01 <ekvm555@naver.com>
2023-09-11 20:20:48 +0900

    refactor: 차트 색상 상수화

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,7 @@
 export const BASE_URL = "";
 export const GET_SEOUL_END_POINT = "/mock/data.json";
+
+export const CHART_COLOR = {
+    BAR: "rgb(39, 158, 255, 0.4)",
+    AREA: "rgb(213, 255, 208, 0.6)",
+};
--- src/utils/getChartDataset.ts
+++ src/utils/getChartDataset.ts
@@ -1,5 +1,6 @@
 import { ChartDataset } from "chart.js";
 import { DistrictSeoulData } from "../types";
+import { CHART_COLOR } from "../constants";
 
 const getBarDataset = (data: DistrictSeoulData[]): ChartDataset<"bar"> => {
     return {
@@ -8,7 +9,7 @@ const getBarDataset = (data: DistrictSeoulData[]): ChartDataset<"bar"> => {
         yAxisID: "bar",
         data: data.map((item) => item.value_bar),
         order: 2,
-        backgroundColor: "rgb(53, 162, 235, 0.6)",
+        backgroundColor: CHART_COLOR.BAR,
     };
 };
 
@@ -19,8 +20,8 @@ const getLineDataset = (data: DistrictSeoulData[]): ChartDataset<"line"> => {
         yAxisID: "area",
         data: data.map((item) => item.value_area),
         order: 1,
-        backgroundColor: "rgb(252, 27, 121, 0.4)",
-        borderColor: "rgb(252, 27, 121)",
+        backgroundColor: CHART_COLOR.AREA,
+        borderColor: CHART_COLOR.AREA,
         borderWidth: 2,
         pointStyle: false,
         fill: true,
