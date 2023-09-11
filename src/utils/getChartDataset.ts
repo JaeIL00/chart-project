3fc1bf5199f192a7d4c268476fcffa48be8fcdaa
ShinJaeIl01 <ekvm555@naver.com>
2023-09-11 20:42:33 +0900

    refactor: 데이터셋 매개변수 수정

--- src/utils/getChartDataset.ts
+++ src/utils/getChartDataset.ts
@@ -2,23 +2,23 @@ import { ChartDataset } from "chart.js";
 import { DistrictSeoulData } from "../types";
 import { CHART_COLOR, CHART_LABElS, CHART_ORDER } from "../constants";
 
-const getBarDataset = (data: DistrictSeoulData[]): ChartDataset<"bar"> => {
+const getBarDataset = (dataArr: DistrictSeoulData[]): ChartDataset<"bar"> => {
     return {
         type: "bar",
         label: CHART_LABElS.BAR,
         yAxisID: CHART_LABElS.BAR,
-        data: data.map((item) => item.value_bar),
+        data: dataArr.map((item) => item.value_bar),
         order: CHART_ORDER.BAR,
         backgroundColor: CHART_COLOR.BAR,
     };
 };
 
-const getLineDataset = (data: DistrictSeoulData[]): ChartDataset<"line"> => {
+const getLineDataset = (dataArr: DistrictSeoulData[]): ChartDataset<"line"> => {
     return {
         type: "line",
         label: CHART_LABElS.AREA,
         yAxisID: CHART_LABElS.AREA,
-        data: data.map((item) => item.value_area),
+        data: dataArr.map((item) => item.value_area),
         order: CHART_ORDER.AREA,
         backgroundColor: CHART_COLOR.AREA,
         borderColor: CHART_COLOR.AREA,
@@ -30,9 +30,9 @@ const getLineDataset = (data: DistrictSeoulData[]): ChartDataset<"line"> => {
 };
 
 const getChartDataset = (
-    data: DistrictSeoulData[]
+    dataArr: DistrictSeoulData[]
 ): ChartDataset<"bar" | "line">[] => {
-    return [getBarDataset(data), getLineDataset(data)];
+    return [getBarDataset(dataArr), getLineDataset(dataArr)];
 };
 
 export default getChartDataset;
